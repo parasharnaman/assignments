194e400feb6ce091b53f4492d06eda62984a7fa4
parasharnaman <155296382+parasharnaman@users.noreply.github.com>
2024-01-22 15:05:51 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,27 @@
 */
 
 function isAnagram(str1, str2) {
+  const newStr1=str1.toLowerCase();
+  const newStr2=str2.toLowerCase();
+  const answer= false;
+  const charCount=[]
+  for(let i=0;i<256;i++){
+    charCount[i]=0;
+  }
+  for(let i=0;i<newStr1.length;i++){
+    charCount[newStr1.charCodeAt(i)]++;
+  }
+  for(let i=0;i<newStr2.length;i++){
+    charCount[newStr2.charCodeAt(i)]--;
+  }
+  
+  for(let i=0;i<256;i++){
+    if(charCount[i]!=0){
+      return false;
+    } 
+  }
 
+  return true;
 }
 
 module.exports = isAnagram;
