e3a5d0c3557b5ff37d9444f7b2c9f9502a9f20cb
parasharnaman <155296382+parasharnaman@users.noreply.github.com>
2024-01-25 01:22:39 +0530

    Update 1-file-cleaner.js

--- week-2/01-async-js/medium/1-file-cleaner.js
+++ week-2/01-async-js/medium/1-file-cleaner.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 
-fs.readFile("file.txt", "utf-8", (err, data) => {
+fs.readFile("file2.txt", "utf-8", (err, data) => {
   data = data.replace(/\s+/g, " ");
   fs.writeFile("file.txt", data, (err) => {
     if (err) throw err;
-    fs.readFile("file.txt", "utf-8", (err, data) => {
+    fs.readFile("file2.txt", "utf-8", (err, data) => {
       console.log(data);
     });
   });
