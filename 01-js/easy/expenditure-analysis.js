12328871b023d7903361284b167780e2a136c481
parasharnaman <155296382+parasharnaman@users.noreply.github.com>
2024-01-24 14:56:37 +0530

    Update expenditure-analysis.js

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -14,7 +14,58 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  // class result{
+  //   constructor(category,totalSpent){
+  //     this.category=category;
+  //     this.price=totalSpent;
+  //   }
+  // }
+  const n = transactions.length;
+  let category1=[];
+  for(let i=0;i<n;i++){
+    if(!(category1.includes(transactions[i].category)))
+      category1.push(transactions[i].category);
+  }
+
+  const price1= new Array(category1.length).fill(0);
+  for(let i=0;i<category1.length;i++){
+    for(let j=0;j<n;j++){
+      if(category1[i]==transactions[j].category){
+          price1[i]=transactions[j].price+price1[i];
+      }
+    }
+  }
+
+  const results= [];
+  for(let i=0;i<category1.length;i++){
+    results.push({"category":category1[i],"totalSpent": price1[i]});
+  }
+
+  return results;
 }
 
+
+
+// calculateTotalSpentByCategory([
+//   {
+//       id: 1,
+//       timestamp: 1656076800000,
+//       price: 10,
+//       category: 'Food',
+//       itemName: 'Pizza',
+//   },
+//   {
+//       id: 2,
+//       timestamp: 1656076800000,
+//       price: 20,
+//       category: 'Shopping',
+//       itemName: 'Clothes',
+//   },
+//   {
+//       id: 3,
+//       timestamp: 1656076800000,
+//       price: 15,
+//       category: 'Food',
+//       itemName: 'Burger',
+//   },]);
 module.exports = calculateTotalSpentByCategory;
