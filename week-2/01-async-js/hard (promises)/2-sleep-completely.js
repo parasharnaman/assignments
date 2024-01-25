54654262617b13baf31f9e7d37f86e3f986d4b4e
parasharnaman <155296382+parasharnaman@users.noreply.github.com>
2024-01-25 09:53:45 +0530

    Update 2-sleep-completely.js

--- week-2/01-async-js/hard (promises)/2-sleep-completely.js
+++ week-2/01-async-js/hard (promises)/2-sleep-completely.js
@@ -17,3 +17,24 @@ function sleep(milliseconds) {
 }
 
 module.exports = sleep;
+
+//this also works
+/*
+ * Write a function that halts the JS thread (make it busy wait) for a given number of milliseconds.
+ * During this time the thread should not be able to do anything else.
+ * the function should return a promise just like before
+ */
+
+function sleep(milliseconds) {
+    const currentdate = new Date();
+    let a =currentdate.getTime();
+    let b=a+milliseconds;
+    while(b>Date.now()){
+
+    }
+    return new Promise(function(ondone){
+        ondone();
+    })
+}
+
+module.exports = sleep;
